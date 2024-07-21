da03c9dd9093625635d7ecfb7428aa14dd4cd544
gaikwadshrutii <gaikwadshrutii01@gmail.com>
2024-07-21 22:03:41 +0530

    initial setup

--- index.js
+++ index.js
@@ -6,20 +6,28 @@ require("dotenv").config()
 mongoose.connect(process.env.MONGO_URL)
 const app = express()
 
-// app.use(express.static(""))
+// app.use(express.static("dist"))
 app.use(express.json())
-app.use(cors(
-    { origin: true }
-))
+app.use(cors({
+    origin: true
+}))
 
-app.use("/api/msg", require("./routes/todo.route"))
+
+app.use("/api", require("./routes/todo.route"))
+
 app.use("*", (req, res) => {
     res.status(404).json({ message: "Resource Not Found" })
 })
+
+
 app.use((error, req, res, next) => {
-    res.status(500).json({ message: error.message || "Something Went Wrong" })
-})
+    console.error("Unhandled error:", error);
+    res.status(500).json({ message: error.message || "Something Went Wrong" });
+});
+
+
 mongoose.connection.once("open", () => {
     app.listen(process.env.PORT, console.log("SERVER RUNNING"))
     console.log("MONGO CONNECTED")
+
 })
--- routes/todo.route.js
+++ routes/todo.route.js
@@ -1,8 +1,8 @@
 const route = require("express").Router()
 
-const { getAllTodos, addTodo } = require("./../controller/todo.controller")
+const { getAllTodos, addTodo } = require("../controller/todo.controller")
 
 route
-    .get("/fetch", getAllTodos)
+    .get("/get", getAllTodos)
     .post("/add", addTodo)
 module.exports = route
